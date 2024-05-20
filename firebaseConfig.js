70d3faddc5f5dd0b6e147d038f2ed19240531eb8
Vincent Franstyo <sorakiriko28814@gmail.com>
2024-05-20 14:10:57 +0700

    feat: setup

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoo_mFsv449XqesF-fklRqAIhjLytwk30",
+  authDomain: "recyco-ppam.firebaseapp.com",
+  projectId: "recyco-ppam",
+  storageBucket: "recyco-ppam.appspot.com",
+  messagingSenderId: "865396782019 ",
+  appId: "1:865396782019:android:0f244060365df497434376"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+        extend: {
+            width: {
+                '38': '150px',
+                '69': '275px',
+            },
+            height: {
+                '38': '150px',
+            },
+            inset: {
+                '49': '198px',
+                '43': '172px',
+                '30': '120px',
+            },
+        },
+    },
+    plugins: [],
+};
+
